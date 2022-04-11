730ee2cf34b8d1345ce2fcab4395572a4ba9c21b
cmunhozprog03 <cmunhozprog03@hotmail.com>
2022-04-11 16:26:09 -0300

    Create UuidTrait

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+
 return new class extends Migration
 {
     /**
@@ -14,7 +15,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
@@ -33,4 +34,6 @@ return new class extends Migration
     {
         Schema::dropIfExists('users');
     }
+
+
 };
